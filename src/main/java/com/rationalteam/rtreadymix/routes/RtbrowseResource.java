58e186f8f2cfa79fd01e8ef96d676d969ad62860
Mutaz Abdullah Hageltom <mutaz@rationalteam.com>
2021-03-28 23:26:26 +0200

    fixed adding new item

--- src/main/java/com/rationalteam/rtreadymix/routes/RtbrowseResource.java
+++ src/main/java/com/rationalteam/rtreadymix/routes/RtbrowseResource.java
@@ -1,19 +1,27 @@
 package com.rationalteam.rtreadymix.routes;
 
 import com.rationalteam.rterp.erpcore.*;
+import com.rationalteam.rterp.erpcore.data.TblProduct;
+import com.rationalteam.rterp.erpcore.data.TblService;
 import com.rationalteam.rtreadymix.*;
+import com.rationalteam.rtreadymix.data.Tblclient;
+import com.rationalteam.rtreadymix.data.Tblnews;
 import com.rationalteam.rtreadymix.purchase.Supplier;
+import com.rationalteam.rtreadymix.purchase.TblSupplier;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import io.quarkus.qute.api.ResourcePath;
+import io.quarkus.vertx.web.Body;
+import io.vertx.core.json.JsonObject;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
-import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriBuilderException;
 import java.text.NumberFormat;
 import java.util.*;
 
@@ -31,8 +39,8 @@ public class RtbrowseResource {
 
     @FormParam("_method")
     String command;
-
-
+    List<Map<String, Object>> rtlist;
+    String table;
     String title;
     List<String> columns = new ArrayList<>();
     String rtype = "";
@@ -85,7 +93,6 @@ public class RtbrowseResource {
 
     private TemplateInstance popdata(String type) {
         TemplateInstance t = template.instance();
-        List<Map<String, Object>> rtlist;
         try {
             List<Map<String, Object>> jar = new ArrayList<>();
             Map<String, Object> filterMap = new HashMap<>();
@@ -147,6 +154,28 @@ public class RtbrowseResource {
         return t;
     }
 
+    @Path("rtbaction")
+    @POST
+    public Response handleView() {
+        TemplateInstance t = browseTemplate.instance();
+        if (command != null && !command.isBlank()) {
+            StringTokenizer tkn = new StringTokenizer(command, "_");
+            String cmd = tkn.countTokens() > 0 ? tkn.nextToken() : "";
+            Integer itemid = tkn.hasMoreTokens() ? Integer.parseInt(tkn.nextToken()) : -1;
+            switch (cmd) {
+                case "view":
+                case "edit":
+                    return Response.seeOther(UriBuilder.fromPath("/readymix/" + rtype).queryParam("itemid", itemid).build()).build();
+                case "addNew":
+                    return Response.seeOther(UriBuilder.fromPath("/readymix/" + rtype).queryParam("itemid", -1).build()).build();
+                default:
+                    title = "nothing to show";
+                    break;
+            }
+        }
+        return Response.notModified().build();
+    }
+
     private void generateListMap(List<Map<String, Object>> rtlist, List<CRtDataObject> cRtDataObjects) {
         NumberFormat nform = NumberFormat.getInstance(Locale.US);
         CExchange xchange = new CExchange();
@@ -177,25 +206,25 @@ public class RtbrowseResource {
 
     }
 
-    @Path("rtbaction")
-    @Produces(MediaType.APPLICATION_JSON)
     @POST
-    public Response handleView() {
-        TemplateInstance t = browseTemplate.instance();
-        if (command != null && !command.isBlank()) {
-            StringTokenizer tkn = new StringTokenizer(command, "_");
-            String cmd = tkn.countTokens() > 0 ? tkn.nextToken() : "";
-            Integer itemid = tkn.hasMoreTokens() ? Integer.parseInt(tkn.nextToken()) : -1;
-            switch (cmd) {
-                case "view":
-                case "edit":
-                    return Response.seeOther(UriBuilder.fromPath("/readymix/" + rtype).queryParam("itemid", itemid).build()).build();
-                default:
-                    title = "nothing to show";
-                    break;
+    @Path("delete")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response delete(@Body JsonObject fbody) {
+        try {
+            if (table == null || fbody.isEmpty())
+                return Response.serverError().entity("Empty Data").build();
+            Integer itemid = fbody.getInteger("itemid");
+            if (table != null && !table.isEmpty()) {
+                boolean b = MezoDB.doSqlIn("delete from ".concat(table).concat(" where id=").concat(itemid.toString()));
+                if (b)
+                    return Response.seeOther(UriBuilder.fromPath("/readymix/rtbrowse/").queryParam("type", rtype).build()).build();
             }
+            return
+                    Response.notModified().build();
+        } catch (IllegalArgumentException | UriBuilderException e) {
+            return Response.serverError().entity(e).build();
         }
-        return Response.notModified().build();
     }
 
     private void popColumns(String type) {
@@ -204,10 +233,12 @@ public class RtbrowseResource {
         switch (type) {
             case "client":
                 Client client = new Client();
+                table = Tblclient.class.getSimpleName();
                 columns.addAll(client.getBrowsable());
                 break;
             case "service":
                 CService service = new CService();
+                table = TblService.class.getSimpleName();
                 columns.addAll(service.getBrowsable());
                 columns.add("unit");
                 columns.add("unitprice");
@@ -216,10 +247,12 @@ public class RtbrowseResource {
                 break;
             case "supplier":
                 Supplier supplier = new Supplier();
+                table = TblSupplier.class.getSimpleName();
                 columns.addAll(supplier.getBrowsable());
                 break;
             case "product":
                 CProduct product = new CProduct();
+                table = TblProduct.class.getSimpleName();
                 columns.addAll(product.getBrowsable());
                 columns.add("unit");
                 columns.add("unitprice");
@@ -227,12 +260,14 @@ public class RtbrowseResource {
                 columns.add("description");
                 break;
             case "news":
+                table = Tblnews.class.getSimpleName();
                 columns.add("id");
                 columns.add("title");
                 columns.add("details");
                 columns.add("ondate");
                 break;
             default:
+                table = null;
                 break;
         }
     }
